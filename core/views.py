53048899ca6bdebd95557dadbf3dcd08a6cfe11f
Kevin Martinez Vargas <s22120203@alumnos.itsur.edu.mx>
2025-10-07 23:05:15 -0600

    añadimos las paginas principales y una autenticacion basica y estructura del frontend

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# core/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Campana # Importamos para mostrar las campañas
+
+def home_view(request):
+    # Obtener un listado de campañas activas (Requisito 12 - Búsqueda/Filtrado Básico)
+    campanas_activas = Campana.objects.filter(estado='ACT').order_by('-fecha_creacion')[:6]
+    
+    context = {
+        'campanas': campanas_activas
+    }
+    return render(request, 'core/home.html', context)
